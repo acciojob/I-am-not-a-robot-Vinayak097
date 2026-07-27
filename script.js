ad3fc1cde9031d0c6f0211cc8fe1ada5628c0038
Vinayak097 <vinayak20injamure@gmail.com>
2026-07-27 11:48:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
 //your code here
 const imgContainer= document.getElementById('img-container');
-const arr=[
+const randomImgn=Math.floor(Math.random() *4);
+const verify=document.getElementById('verify')
+const reset=document.getElementById('reset')
+const h=document.getElementById('h')
+
+
+let selectedimg=[];
+renderMain()
+function renderMain(){
+	imgContainer.innerHTML=""
+	h.textContent="Please click on the identical tiles to verify that you are not a robot."
+	verify.style.display='none'
+reset.style.display='none'
 	
-]
-for(let i=0;i<5;i++){
+	
+for(let i=1;i<=5;i++){
 	const imgtag=document.createElement('img')
 	imgContainer.appendChild(imgtag)
-	imgtag.alt='image'
-	imgtag.src=arr[i]
+	imgtag.alt='image';
+	imgtag.classList.add(`img${i}`)
+	imgtag.style.cursor='pointer'
+	addEventclickforimage(imgtag)
+}
+	addsiximg()
 	
 }
+
+
+function addsiximg(){
+	const imgtag=document.createElement('img')
+	imgtag.classList.add(`img${randomImgn}`)
+	imgContainer.appendChild(imgtag)
+	imgtag.innerHTML=randomImgn;
+	addEventclickforimage(imgtag)
+	
+}
+verify.addEventListener('click',()=>{
+	if(selectedimg.length==2){
+		if(selectedimg[0]==selectedimg[1]){
+			h.textContent="You are a human. Congratulations!"
+		}else{
+			h.textContent="We can't verify you as a human. You selected the non-identical tiles."
+			reset.style.display='block'
+		}
+	}
+	
+})
+reset.addEventListener('click',()=>{
+	
+	selectedimg=[]
+	renderMain()
+	
+})
+function addEventclickforimage(imgtag){
+	imgtag.addEventListener('click',()=>{
+		
+		reset.style.display='inline'
+		if(selectedimg.length!=2){
+			h.textContent=""
+			imgtag.style.padding='2px'
+			classN=imgtag.className;
+			
+			selectedimg.push(imgtag.className)
+		}
+		if(selectedimg.length==2){
+			
+			
+				
+				verify.style.display='inline'
+			
+			
+		}
+		
+	})
+}
+
